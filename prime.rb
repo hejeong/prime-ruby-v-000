0ff2ead63d5f5195da9bac09e6b9d66c703760ef
Jonathan Hong <jonhong777@gmail.com>
2018-07-04 03:27:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,5 +4,10 @@ def prime?(integer)
     return false
   else 
     factors = (2..integer-1).to_a
-    
+    factors.each do |factor|
+      if integer % factor == 0
+        return false
+      end
+    end
+end
 end
